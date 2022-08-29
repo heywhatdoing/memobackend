9156290a81ec872de415f8d1cfdf2d87815feb88
DESKTOP-OCR7289\jiape <jiapengbai@gmail.com>
2022-08-28 20:28:53 -0700

    no message

--- backend/settings.py
+++ backend/settings.py
@@ -68,7 +68,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
 
 ROOT_URLCONF = 'backend.urls'
 
